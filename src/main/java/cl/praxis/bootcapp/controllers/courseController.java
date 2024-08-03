b19a7efcb191aaa62b42e8e74b36898836d47c02
Bryan Cabrera <bryancabreram3@gmail.com>
2024-08-02 22:41:34 -0400

    add: Adding routes and crud methods

--- src/main/java/cl/praxis/bootcapp/controllers/courseController.java
+++ src/main/java/cl/praxis/bootcapp/controllers/courseController.java
@@ -1,31 +1,47 @@
 package cl.praxis.bootcapp.controllers;
 
 import cl.praxis.bootcapp.entities.Course;
+import cl.praxis.bootcapp.services.IBaseServiceCRUD;
+import cl.praxis.bootcapp.services.ICourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/course")
 public class courseController {
+    @Autowired
+    private IBaseServiceCRUD<Course> courseService;
 
     // ---------- ROUTES ----------
-    @GetMapping("/view")
-    public String courseView() {
+    @GetMapping("/courses")
+    public String courseView(Model model) {
+        model.addAttribute("courses", courseService.getAll());
         return "course-view";
     }
 
-    @GetMapping("/form")
-    public String newPatient() {
+    @GetMapping("/course-form")
+    public String newCourse() {
         return "course-form";
     }
 
     // ---------- CRUD ---------
-    @PostMapping("/patients")
-    public String addPatient(@ModelAttribute Course course) {
+    @PostMapping("/add-course")
+    public String addCourse(@ModelAttribute Course course) {
+        courseService.create(course);
         return "redirect:/course-view";
     }
-    @PutMapping()
-    public String updatePatient(@ModelAttribute Course course) {
+    @PutMapping("/update-course")
+    public String updateCourse(@ModelAttribute Course course) {
+        courseService.update(course);
+        return "redirect:/course-view";
+    }
+
+    @GetMapping("/delete-course")
+    public String deleteCourse(@RequestParam Long id) {
+        courseService.delete(id);
         return "redirect:/course-view";
     }
 }
