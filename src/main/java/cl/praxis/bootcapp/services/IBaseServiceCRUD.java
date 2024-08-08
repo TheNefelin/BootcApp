5f3f87c8995e0c45e844e75bffc0574e7831f2ef
Francisco Carmona <flcarmonac@yahoo.com>
2024-08-08 00:08:15 -0400

    Arreglando Cachos

--- src/main/java/cl/praxis/bootcapp/services/IBaseServiceCRUD.java
+++ src/main/java/cl/praxis/bootcapp/services/IBaseServiceCRUD.java
@@ -5,9 +5,6 @@ import cl.praxis.bootcapp.entities.GradeDTO;
 import java.util.List;
 
 public interface IBaseServiceCRUD<T> {
-    // obtiene Notas con los OBJ Teacher, Student, Subject en una listaDTO
-    List<GradeDTO> getAllNote();
-
     List<T> getAll();
     T getById(Long id);
     T create(T t);
