b5c2f6348ea416d52fa2ad812f0bdb516194deb2
Tamara López <tamaralopez3@gmail.com>
2024-08-08 20:48:17 -0400

    Se crea vista courseList, controlador con botón editar directamente en lista

--- src/main/java/cl/praxis/bootcapp/controllers/CourseController.java
+++ src/main/java/cl/praxis/bootcapp/controllers/CourseController.java
@@ -3,11 +3,9 @@ import cl.praxis.bootcapp.entities.Course;
 import cl.praxis.bootcapp.services.imp.CourseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.Optional;
 
 @Controller
@@ -20,9 +18,10 @@ public class CourseController  {
         model.addAttribute("courses", courseService.getAll());
         return "courseList";
     }
-    @PostMapping("/course/edit/{id}")
-    public String updateCourse(@PathVariable("id") Long id, Model model){
-    model.addAttribute("editcourse", courseService);
-    return "courseList";
+  @PostMapping("/course/update")
+    public String updateCourse(@ModelAttribute Course course, Model model){
+        courseService.update(course);
+        model.addAttribute("courses", courseService.getAll());
+        return "courseList";
     }
 }
