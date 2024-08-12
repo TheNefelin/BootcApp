9470fc9dbda2d2a24b96d0b1e6c322f59982e64f
Ion <ioncucer@gmail.com>
2024-08-12 17:52:21 -0400

    Comit del Rest Controller

--- src/main/java/cl/praxis/bootcapp/controllers/RoleRestController.java
+++ src/main/java/cl/praxis/bootcapp/controllers/RoleRestController.java
@@ -1,4 +1,65 @@
 package cl.praxis.bootcapp.controllers;
 
-public class RolRestControler {
+import cl.praxis.bootcapp.entities.Role;
+import cl.praxis.bootcapp.services.imp.RoleServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/roles")
+public class RoleRestController {
+
+    private final RoleServiceImp roleService;
+
+    @Autowired
+    public RoleRestController(RoleServiceImp roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public List<Role> getAllRoles() {
+        return roleService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Role> getRoleById(@PathVariable Long id) {
+        Role role = roleService.getById(id);
+        if (role != null) {
+            return ResponseEntity.ok(role);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Role> createRole(@RequestBody Role role) {
+        Role createdRole = roleService.create(role);
+        return ResponseEntity.ok(createdRole);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Role> updateRole(@PathVariable Long id, @RequestBody Role roleDetails) {
+        Role existingRole = roleService.getById(id);
+        if (existingRole != null) {
+            existingRole.setName(roleDetails.getName());
+            existingRole.setActive(roleDetails.getActive());
+            Role updatedRole = roleService.update(existingRole);
+            return ResponseEntity.ok(updatedRole);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRole(@PathVariable Long id) {
+        if (roleService.getById(id) != null) {
+            roleService.delete(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
